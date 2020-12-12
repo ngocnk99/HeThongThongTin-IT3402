bd48a4af6873f74881cc2990836b9836748700a2
ngocnk99 <ngocnk99@gmail.com>
2020-12-12 13:40:06 +0700

    ok

--- server/app/controllers/dist/book.controller.dev.js
+++ server/app/controllers/dist/book.controller.dev.js
@@ -1,64 +1,8 @@
 "use strict";
 
-var config = require("../config/auth.config");
-
 var db = require("../models");
 
 var User = db.user;
-var Role = db.role;
-
-var jwt = require("jsonwebtoken");
-
-var bcrypt = require("bcryptjs");
-
-exports.signin = function (req, res) {
-  console.log(req.body);
-  User.findOne({
-    username: req.body.username
-  }).populate("roles", "-__v").exec(function (err, user) {
-    if (err) {
-      res.status(500).send({
-        message: err
-      });
-      return;
-    }
-
-    if (!user) {
-      return res.status(404).send({
-        message: "User Not found."
-      });
-    }
-
-    var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
-
-    if (!passwordIsValid) {
-      return res.status(401).send({
-        accessToken: null,
-        message: "Invalid Password!"
-      });
-    }
-
-    var token = jwt.sign({
-      id: user.id
-    }, config.secret, {
-      expiresIn: 86400 // 24 hours
-
-    });
-    var authorities = [];
-
-    for (var i = 0; i < user.roles.length; i++) {
-      authorities.push("ROLE_" + user.roles[i].name.toUpperCase());
-    }
-
-    res.status(200).send({
-      id: user._id,
-      username: user.username,
-      email: user.email,
-      roles: authorities,
-      accessToken: token
-    });
-  });
-};
 
 exports.addBook = function (req, res) {
   console.log(req.body.email);
